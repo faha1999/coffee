2d85b65c915f4ce21c9e62beb3f61cb58bca13c0
faha1999 <kafahad1999@gmail.com>
2022-03-02 20:47:59 +0600

    added onscroll & header border function ; update ui

--- app/js/app.js
+++ app/js/app.js
@@ -33,3 +33,37 @@ window.onscroll = () => {
   searchForm.classList.remove('active');
   cartItem.classList.remove('active');
 };
+
+// Header border
+
+function scrollHeader() {
+  const nav = document.getElementById('header');
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add('active-border');
+  else nav.classList.remove('active-border');
+}
+window.addEventListener('scroll', scrollHeader);
+
+// Scroll active
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.navbar a[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('.navbar a[href*=' + sectionId + ']')
+        .classList.remove('active');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
